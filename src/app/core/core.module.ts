82af042825deb803a2659b2ae8b614b482074099
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-02-05 20:40:55 -0800

    Lecture 280 - restructure modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,6 +5,11 @@ import { HomeComponent } from "./home/home.component";
 import { HeaderComponent } from "./header/header.component";
 import { SharedModule } from "../shared/shared.module";
 import { AppRoutingModule } from "../app-routing.module";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { RecipeService } from "../recipes/recipe.service";
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+
 
 @NgModule({
   declarations: [
@@ -18,6 +23,12 @@ import { AppRoutingModule } from "../app-routing.module";
   exports: [
     AppRoutingModule,
     HeaderComponent
+  ],
+  providers: [
+    ShoppingListService,
+    RecipeService,
+    DataStorageService,
+    AuthService
   ]
 })
 export class CoreModule {}
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -14,6 +14,7 @@ import { SharedModule } from "../shared/shared.module";
 
 
 
+
 @NgModule({
   declarations: [
     RecipesComponent,
