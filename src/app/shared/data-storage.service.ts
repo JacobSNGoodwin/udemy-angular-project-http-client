ead14d0ebc2bda89cb74052da5e547f30f8220f5
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-02-12 18:58:34 -0800

    Lecture 294 - set Http params using HttpClient

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -16,8 +16,9 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
     //const headers = new HttpHeaders().set('Authorization', 'dafadsfkhj');
-    return this.httpClient.put('https://ng-recipe-book-1a4ce.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes(), {
+    return this.httpClient.put('https://ng-recipe-book-1a4ce.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
       observe: 'body',
+      params: new HttpParams().set('auth', token)
       //headers: headers
     });
   }
