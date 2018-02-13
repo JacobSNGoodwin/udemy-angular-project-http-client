4cf30cf7cf00e03ae5a5c690fa5396a5013f1c44
Jacob Goodwin <jacob.goodwin@gmail.com>
2018-02-12 21:52:00 -0800

    Lecture 295 - view request progress

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from "@angular/common/http";
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
@@ -16,11 +16,17 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
     //const headers = new HttpHeaders().set('Authorization', 'dafadsfkhj');
-    return this.httpClient.put('https://ng-recipe-book-1a4ce.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
-      observe: 'body',
+    // return this.httpClient.put('https://ng-recipe-book-1a4ce.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+    //   observe: 'body',
+    //   params: new HttpParams().set('auth', token)
+    //   //headers: headers
+    // });
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-1a4ce.firebaseio.com/recipes.json', this.recipeService.getRecipes(),{
+      reportProgress:true,
       params: new HttpParams().set('auth', token)
-      //headers: headers
     });
+
+    return this.httpClient.request(req);
   }
 
   getRecipes() {
